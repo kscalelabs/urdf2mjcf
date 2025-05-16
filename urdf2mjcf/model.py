b1d7e20ac1874da41a24ac26244625689f30a484
id <i.dyke@proton.me>
2025-05-16 10:44:53 -0700

    Clearer understanding of solimp, solref

--- urdf2mjcf/model.py
+++ urdf2mjcf/model.py
@@ -90,11 +90,12 @@ class ExplicitFloorContacts(BaseModel):
 class WeldConstraint(BaseModel):
     """Represents a weld constraint between two bodies.
     Useful for locking bodies in the air (suspending). By default, body2 is set to "world" to create a fixed constraint in global space.
+    Default parameters approximately lock the body rigidly in place -- stiffer than Mujoco defaults.
     """
     
     body1: str
     body2: str = "world"
-    solimp: list[float] = [0.9, 0.95, 0.005] # clamped to 0.005 for higher stiffness
+    solimp: list[float] = [0.95, 0.99, 0.005, 0.5, 2] # Very tight width, aggressive response.
     solref: list[float] = [0.005, 1.0] # critical damping 1.0, very quick time constant to return to static
 
 
